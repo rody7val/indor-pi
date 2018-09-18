6c2fa81cd53c462b0f34e7f59161d32e7faf80f9
unknown <rod7val@gmail.com>
2018-09-18 19:25:29 -0300

    primer commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const firebase = require('firebase');
+const five = require('johnny-five');
+const Raspi = require('raspi-io');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCx63dO4hmqDjUphM11zba_1ONMcPekQNc",
+  authDomain: "indor-pi.firebaseapp.com",
+  databaseURL: "https://indor-pi.firebaseio.com",
+  projectId: "indor-pi",
+  storageBucket: "indor-pi.appspot.com",
+  messagingSenderId: "407116836792"
+});
+var board = new five.Board({ 
+	io: new Raspi()
+});
+
+board.on('ready', () => {
+  var servo = new five.Servo({ pin: 'GPIO02' });
+	firebase.database().ref('servo').on('value').then(snapshot => {
+  	servo.to(snapshot.val());
+	});
+});
