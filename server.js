766b4c0e27d2b8788558d4bd4f02c206f7100f3f
unknown <rod7val@gmail.com>
2018-09-19 19:46:36 -0300

    1er prueba

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const firebase = require('firebase');
 const five = require('johnny-five');
-const Raspi = require('raspi-io');
+// const Raspi = require('raspi-io');
 
 firebase.initializeApp({
   apiKey: "AIzaSyCx63dO4hmqDjUphM11zba_1ONMcPekQNc",
@@ -10,13 +10,16 @@ firebase.initializeApp({
   storageBucket: "indor-pi.appspot.com",
   messagingSenderId: "407116836792"
 });
-var board = new five.Board({ 
-	io: new Raspi()
-});
+
+var board = new five.Board();
+// var board = new five.Board({ 
+	// io: new Raspi()
+// });
 
 board.on('ready', () => {
-  var servo = new five.Servo({ pin: 'GPIO02' });
-	firebase.database().ref('servo').on('value').then(snapshot => {
-  	servo.to(snapshot.val());
+ //  var servo = new five.Servo({ pin: 'GPIO02' });
+  firebase.database().ref('servo').on('value').then(snapshot => {
+    console.log(snapshot.val())
+  	// servo.to(snapshot.val());
 	});
 });
