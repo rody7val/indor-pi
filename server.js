701f9e4f43291da67afbdf91d705adc33ce1c6a1
unknown <rod7val@gmail.com>
2018-09-19 19:56:35 -0300

    asegurando que el error sea por firebase

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-const firebase = require('firebase');
-// const five = require('johnny-five');
-// const Raspi = require('raspi-io');
+// const firebase = require('firebase');
+const five = require('johnny-five');
+const Raspi = require('raspi-io');
 
-firebase.initializeApp({
-  apiKey: "AIzaSyCx63dO4hmqDjUphM11zba_1ONMcPekQNc",
-  authDomain: "indor-pi.firebaseapp.com",
-  databaseURL: "https://indor-pi.firebaseio.com",
-  projectId: "indor-pi",
-  storageBucket: "indor-pi.appspot.com",
-  messagingSenderId: "407116836792"
-});
+// firebase.initializeApp({
+  // apiKey: "AIzaSyCx63dO4hmqDjUphM11zba_1ONMcPekQNc",
+  // authDomain: "indor-pi.firebaseapp.com",
+  // databaseURL: "https://indor-pi.firebaseio.com",
+  // projectId: "indor-pi",
+  // storageBucket: "indor-pi.appspot.com",
+  // messagingSenderId: "407116836792"
+// });
 
 // var board = new five.Board();
-// var board = new five.Board({ 
-	// io: new Raspi()
-// });
+var board = new five.Board({ 
+	io: new Raspi()
+});
 
-// board.on('ready', () => {
- //  var servo = new five.Servo({ pin: 'GPIO02' });
-  firebase.database().ref('servo').on('value').then(snapshot => {
-    console.log(snapshot.val())
-  	// servo.to(snapshot.val());
-	});
-// });
+board.on('ready', () => {
+  var servo = new five.Servo({ pin: 'GPIO02' });
+  // firebase.database().ref('servo').on('value').then(snapshot => {
+    // console.log(snapshot.val())
+  	servo.to(90);
+	// });
+});
