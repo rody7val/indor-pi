ba3bbaeaf3180030fb10c86d9cb94a72958b8987
unknown <rod7val@gmail.com>
2018-09-19 21:16:09 -0300

    agregado firebase-admin

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-// const firebase = require('firebase');
+var firebase = require('firebase-admin');
 var five = require('johnny-five');
 var Raspi = require('raspi-io');
 
-// firebase.initializeApp({
-  // apiKey: "AIzaSyCx63dO4hmqDjUphM11zba_1ONMcPekQNc",
-  // authDomain: "indor-pi.firebaseapp.com",
-  // databaseURL: "https://indor-pi.firebaseio.com",
-  // projectId: "indor-pi",
-  // storageBucket: "indor-pi.appspot.com",
-  // messagingSenderId: "407116836792"
-// });
+firebase.initializeApp({
+  apiKey: "AIzaSyCx63dO4hmqDjUphM11zba_1ONMcPekQNc",
+  authDomain: "indor-pi.firebaseapp.com",
+  databaseURL: "https://indor-pi.firebaseio.com",
+  projectId: "indor-pi",
+  storageBucket: "indor-pi.appspot.com",
+  messagingSenderId: "407116836792"
+});
 
 // var board = new five.Board();
 var board = new five.Board({ 
@@ -17,16 +17,10 @@ var board = new five.Board({
 });
 
 board.on('ready', () => {
-  
-  var led = new five.Led("P1-11");
-  led.blink();
-  // var servo = new five.Servo('P1-11');
-
-  // this.repl.inject({
-  //   servo: servo
-  // });
-  // firebase.database().ref('servo').on('value').then(snapshot => {
-    // console.log(snapshot.val())
-  	// servo.to(90);
-	// });
+  var db = firebase.database();
+  // var led = new five.Led("P1-11");
+  // led.blink();
+  db.ref('servo').on('value').then(snapshot => {
+    console.log(snapshot.val())
+	});
 });
