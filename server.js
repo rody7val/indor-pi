62922dc8ba34dccb049fe141b599eb7cd4f9f6f9
unknown <rod7val@gmail.com>
2018-09-20 21:46:53 -0300

    quitar var entorno

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
-require('dotenv').config()
+// require('dotenv').config()
 const firebase = require('firebase-admin');
 const five = require('johnny-five');
 const Raspi = require('raspi-io');
+const serviceAccount = require("./serviceAccountKey.json");
 
 firebase.initializeApp({
   databaseURL: "https://indor-pi.firebaseio.com",
-  credential: firebase.credential.cert({
-    "type": "service_account",
-    "project_id": "indor-pi",
-    "private_key_id": process.env.private_key_id,
-    "private_key": process.env.private_key,
-    "client_email": process.env.client_email,
-    "client_id": process.env.client_id,
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-rwhpo%40indor-pi.iam.gserviceaccount.com"
-  }),
+  credential: firebase.credential.cert(serviceAccount),
+  // credential: firebase.credential.cert({
+  //   "type": "service_account",
+  //   "project_id": "indor-pi",
+  //   "private_key_id": process.env.private_key_id,
+  //   "private_key": process.env.private_key,
+  //   "client_email": process.env.client_email,
+  //   "client_id": process.env.client_id,
+  //   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  //   "token_uri": "https://oauth2.googleapis.com/token",
+  //   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  //   "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-rwhpo%40indor-pi.iam.gserviceaccount.com"
+  // }),
 });
 
 const board = new five.Board({ 
